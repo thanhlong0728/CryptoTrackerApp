4e949e886da1f9316d4a353c01a2e37e385cb995
LongThV <longvt@flydinotech.com>
2024-07-13 22:49:12 +0700

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Mac on 13/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
